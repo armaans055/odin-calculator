3f480fde50590e8ee73b413199a99ced0649d8ca
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-14 11:37:01 -0800

    Started html and css for calculator

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello world")
+let total = 0;
+
+let add = (x,y) => {return x+y};
+let subtract = (x,y) => {return x-y};
+let multiply = (x,y) => {return x*y};
+let divide = function(x,y){
+    if(y===0){
+        return "Error"
+    }else{
+        return x/y
+    }
+}
