0e505d95fa7fdc7bb5c3e9b9e463190500b50823
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-14 15:06:07 -0800

    Made listeners for all numbers

--- app.js
+++ app.js
@@ -1,10 +1,68 @@
-let total = 0;
+let total = '';
+let displayTotal = document.querySelector('.computed-total')
+let zero = document.querySelector('.zero')
+let one = document.querySelector('.one')
+let two = document.querySelector('.two')
+let three = document.querySelector('.three')
+let four = document.querySelector('.four')
+let five = document.querySelector('.five')
+let six = document.querySelector('.six')
+let seven = document.querySelector('.seven')
+let eight = document.querySelector('.eight')
+let nine = document.querySelector('.nine')
+let addButton = document.querySelector('.add')
+let subButton = document.querySelector('.sub')
+let divButton = document.querySelector('.divi')
+let mulButton = document.querySelector('.mul')
 
-let add = (x,y) => {return x+y};
+
+one.addEventListener('click', ()=>{
+    total += '1'
+    displayTotal.textContent = total
+})
+two.addEventListener('click', ()=>{
+    total += '2'
+    displayTotal.textContent = total
+})
+three.addEventListener('click', ()=>{
+    total += '3'
+    displayTotal.textContent = total
+})
+four.addEventListener('click', ()=>{
+    total += '4'
+    displayTotal.textContent = total
+})
+five.addEventListener('click', ()=>{
+    total += '5'
+    displayTotal.textContent = total
+})
+six.addEventListener('click', ()=>{
+    total += '6'
+    displayTotal.textContent = total
+})
+seven.addEventListener('click', ()=>{
+    total += '7'
+    displayTotal.textContent = total
+})
+eight.addEventListener('click', ()=>{
+    total += '8'
+    displayTotal.textContent = total
+})
+nine.addEventListener('click', ()=>{
+    total += '9'
+    displayTotal.textContent = total
+})
+zero.addEventListener('click', ()=>{
+    total += '0'
+    displayTotal.textContent = total
+})
+
+
+let addition = (x,y) => {return x+y};
 let subtract = (x,y) => {return x-y};
 let multiply = (x,y) => {return x*y};
 let divide = function(x,y){
-    if(y===0){
+    if(y === 0){
         return "Error"
     }else{
         return x/y
