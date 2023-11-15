52144ad5081a9e3343a2d9bd49eb535d9a4a48e7
Armaan Sandhu <armaansandhuuu209@gmail.com>
2023-11-15 00:26:15 -0800

    Added functions for multi and add

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-let total = '';
-let total2 = '';
+let operand1 = '';
+let operand2 = '';
+let operatorClicked = false;
+let equalsClicked = false;
+let plusClicked = false;
+let minusClicked = false;
+let divClicked = false;
+let multiClicked = false;
 let displayTotal = document.querySelector('.computed-total')
 let oldTotal = displayTotal.innerHTML
 let zero = document.querySelector('.zero')
@@ -19,61 +25,215 @@ let mulButton = document.querySelector('.mul')
 let clear = document.querySelector('.clear')
 let equals = document.querySelector('.equals')
 clear.addEventListener('click',()=>{
-    total = ''
+    operand1 = ''
+    operand2 = ''
     displayTotal.textContent = oldTotal
+    operatorClicked = false;
+    equalsClicked = false;
 })
+equals.addEventListener('click', ()=>{
+    equalsClicked = true;
+})
+equals.addEventListener('click', equalsButtonPressed)
+zero.addEventListener('click', buttonZeroPressed)
+one.addEventListener('click', buttonOnePressed)
+two.addEventListener('click', buttonTwoPressed)
+three.addEventListener('click', buttonThreePressed)
+four.addEventListener('click', buttonFourPressed)
+five.addEventListener('click', buttonFivePressed)
+six.addEventListener('click', buttonSixPressed)
+seven.addEventListener('click', buttonSevenPressed)
+eight.addEventListener('click', buttonEightPressed)
+nine.addEventListener('click', buttonNinePressed)
+addButton.addEventListener('click', ()=> {operatorClicked = true})
+mulButton.addEventListener('click', ()=> {operatorClicked = true})
+divButton.addEventListener('click', ()=> {operatorClicked = true})
+subButton.addEventListener('click', ()=> {operatorClicked = true})
+addButton.addEventListener('click' , buttonAddPressed)
+subButton.addEventListener('click', buttonMinusPressed)
+mulButton.addEventListener('click', buttonMultiPressed)
 function operate(a,b,operand){
-    a = Number(a)
-    b = Number(b)
+    a = parseInt(a)
+    b = parseInt(b)
     switch(operand){
         case '+':
             return addition(a,b)
-            
+        case '-':
+            return subtract(a,b)
+        case '/':
+            return divide(a,b)
+        case '*':
+            return multiply(a,b)
+    }
+}
 
+function buttonOnePressed(){
+    if(operatorClicked === false){
+        operand1 += '1'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '1'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonTwoPressed(){
+    if(operatorClicked === false){
+        operand1 += '2'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '2'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonThreePressed(){
+    if(operatorClicked === false){
+        operand1 += '3'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '3'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonFourPressed(){
+    if(operatorClicked === false){
+        operand1 += '4'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '4'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonFivePressed(){
+    if(operatorClicked === false){
+        operand1 += '5'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '5'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonSixPressed(){
+    if(operatorClicked === false){
+        operand1 += '6'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '6'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonSevenPressed(){
+    if(operatorClicked === false){
+        operand1 += '7'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '7'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonEightPressed(){
+    if(operatorClicked === false){
+        operand1 += '8'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '8'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonNinePressed(){
+    if(operatorClicked === false){
+        operand1 += '9'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '9'
+        displayTotal.textContent = operand2
+    }
+}
+function buttonZeroPressed(){
+    if(operatorClicked === false){
+        operand1 += '0'
+        displayTotal.textContent = operand1
+    }else{
+        operand2 += '0'
+        displayTotal.textContent = operand2
+    }
+}
+// function buttonAddPressed(){
+//         let tempVal1 = +operand1
+//     let tempVal2 = +operand2
+//     operand1 = tempVal1 + tempVal2
+//     operand1 = operand1.toString()
+//     operand2 = ''
+// }
+
+
+function buttonAddPressed(){
+    plusClicked = true;
+    let tempVal1 = +operand1
+    let tempVal2 = +operand2
+    operand1 = tempVal1 + tempVal2
+    operand1 = operand1.toString()
+    operand2 = ''
+    displayTotal.textContent = operand1
+}
+
+function buttonMinusPressed(){
+    minusClickedClicked = true;
+    let tempVal1 = +operand1
+    let tempVal2 = +operand2
+    operand1 = tempVal1 - tempVal2
+    operand1 = operand1.toString()
+    operand2 = ''
+    displayTotal.textContent = operand1
+}
+
+function buttonMultiPressed(){
+    multiClicked = true;
+    let tempVal1 = +operand1
+    let tempVal2 = +operand2
+    operand1 = tempVal1 * tempVal2
+    operand1 = operand1.toString()
+    operand2 = ''
+    displayTotal.textContent = operand1
+}
+function buttonDivPressed(){
+    plusClicked = true;
+    let tempVal1 = +operand1
+    let tempVal2 = +operand2
+    operand1 = tempVal1 + tempVal2
+    operand1 = operand1.toString()
+    operand2 = ''
+    displayTotal.textContent = operand1
+}
+function equalsButtonPressed(){
+    if (plusClicked === true){
+        operand1 = +operand1 + +operand2
+        operand1 = operand1.toString()
+        displayTotal.textContent = operand1
+        plusClicked = false;
+    }else if(minusClicked === true){
+        operand1 = +operand1 - +operand2
+        operand1 = operand1.toString()
+        displayTotal.textContent = operand1
+        plusClicked = false;
+    }else if(divClicked === true){
+        operand1 = +operand1 / +operand2
+        operand1 = operand1.toString()
+        displayTotal.textContent = operand1
+        plusClicked = false;
+    }else if(multiClicked === true){
+        operand1 = +operand1 * +operand2
+        operand1 = operand1.toString()
+        displayTotal.textContent = operand1
+        plusClicked = false;
+    }else{
+        console.log("critical error")
     }
 }
 
 
-one.addEventListener('click', ()=>{
-    total += '1'
-    displayTotal.textContent = total
-})
-two.addEventListener('click', ()=>{
-    total += '2'
-    displayTotal.textContent = total
-})
-three.addEventListener('click', ()=>{
-    total += '3'
-    displayTotal.textContent = total
-})
-four.addEventListener('click', ()=>{
-    total += '4'
-    displayTotal.textContent = total
-})
-five.addEventListener('click', ()=>{
-    total += '5'
-    displayTotal.textContent = total
-})
-six.addEventListener('click', ()=>{
-    total += '6'
-    displayTotal.textContent = total
-})
-seven.addEventListener('click', ()=>{
-    total += '7'
-    displayTotal.textContent = total
-})
-eight.addEventListener('click', ()=>{
-    total += '8'
-    displayTotal.textContent = total
-})
-nine.addEventListener('click', ()=>{
-    total += '9'
-    displayTotal.textContent = total
-})
-zero.addEventListener('click', ()=>{
-    total += '0'
-    displayTotal.textContent = total
-})
+
+
+
 
 
 let addition = (x,y) => {return x+y};
@@ -85,4 +245,9 @@ let divide = function(x,y){
     }else{
         return x/y
     }
+}
+
+
+function buttonOne(){
+
 }
